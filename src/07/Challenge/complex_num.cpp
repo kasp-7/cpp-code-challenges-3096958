2124b774aed30bdd2ea0c40b8e4068c8a716ce11
kasp-7 <olehpm@gmail.com>
2025-03-05 17:48:12 +0000

    Challenge 7

--- src/07/Challenge/complex_num.cpp
+++ src/07/Challenge/complex_num.cpp
@@ -1,33 +1,37 @@
 #include "complex_num.h"
 
-complex_num::complex_num(double r, double i){
+complex_num::complex_num(double r, double i) :
+m_r(r),
+m_i(i)
+{
     // Write your code here
 }
 
-complex_num complex_num::operator +(complex_num w){
-    complex_num z;
+complex_num complex_num::operator +(const complex_num& w){
+    complex_num z(m_r + w.m_r, m_i + w.m_i);
     // Write your code here
     return z;
 }
 
-complex_num complex_num::operator -(complex_num w){
-    complex_num z;
+complex_num complex_num::operator -(const complex_num& w){
+    complex_num z(m_r - w.m_r, m_i - w.m_i);
     // Write your code here
     return z;
 }
 
-complex_num complex_num::operator *(complex_num w){
-    complex_num z;
-    // Write your code here
-    return z;
-}
+// complex_num complex_num::operator *(complex_num w){
+//     complex_num z;
+//     // Write your code here
+//     return z;
+// }
 
-complex_num complex_num::operator /(complex_num w){
-    complex_num z;
-    // Write your code here
-    return z;
-}
+// complex_num complex_num::operator /(complex_num w){
+//     complex_num z;
+//     // Write your code here
+//     return z;
+// }
 
 void complex_num::print(std::ostream &os){
     // Write your code here
+    os << '(' << m_r << (m_i < 0 ? " - " : " + ") << std::abs(m_i) << 'i)' << std::endl;
 }
--- src/07/Challenge/complex_num.h
+++ src/07/Challenge/complex_num.h
@@ -6,13 +6,15 @@
 class complex_num{
     
     // Write your code here
+    double m_r;
+    double m_i;
 
     public:
          complex_num(double = 0.0, double = 0.0);
-         complex_num operator +(complex_num);
-         complex_num operator -(complex_num);
-         complex_num operator *(complex_num);
-         complex_num operator /(complex_num);
+         complex_num operator +(const complex_num&);
+         complex_num operator -(const complex_num&);
+         //complex_num operator *(complex_num);
+         //complex_num operator /(complex_num);
          void print(std::ostream &os);
 };
 
